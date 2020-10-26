cf4a4bab339e43a7cfd1932cefe6a6a5978b29f0
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-26 13:53:31 -0700

    commit tree ebed47c2333e7dcba60474924757a9c23d58816a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
 
 
+class Book
+    def initialize(title)
+        @title = title
+    end
+
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+
+end
+
+Book.new("And Then There Were None")
